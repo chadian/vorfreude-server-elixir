12c48568748be04837cbf5b364efd86e7449d91b
Chad Carbert <chadcarbert@me.com>
2022-04-07 16:31:56 -0400

    Add empty result set for missing or invalid api key

--- lib/vorfreude_web/controllers/images_controller.ex
+++ lib/vorfreude_web/controllers/images_controller.ex
@@ -17,14 +17,21 @@ defmodule VorfreudeWeb.ImagesController do
   end
 
   defp results(conn, {:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
-    json(conn, Jason.decode!(body))
+    missing_api_key_code = 100
+
+    with %{"code" => missing_api_key_code} <- Jason.decode!(body) do
+      Logger.error("Missing or invalid API KEY, ensure it's correct and set as an environment variable")
+      empty_result(conn)
+    else
+      result ->
+        json(conn, result)
+    end
   end
 
   defp results(conn, {:error, error}) do
     Logger.error("Failed with error:")
     Logger.error(error)
-    empty_result = %{"photos" => %{"photo" => []}}
-    json(conn, empty_result)
+    empty_result(conn)
   end
 
   defp flickr_search_url(searchTerms) do
@@ -45,6 +52,11 @@ defmodule VorfreudeWeb.ImagesController do
 
     encoded_query = URI.encode_query(query)
     @flickr_api_url <> "?" <> encoded_query
+  end
+
+  defp empty_result(conn)  do
+    empty_result = %{"photos" => %{"photo" => []}}
+    json(conn, empty_result)
   end
 
   defp flickr_api_key do
